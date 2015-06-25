4135f93b6ece107615597c69a2dce2d3f4a96009
Cody Buffaloe <codylbuffaloe@gmail.com>
2015-06-24 20:28:58 -0400

    third trial, declared variable outside loop, .next inside...

--- Code/PoppinBottles.rb
+++ Code/PoppinBottles.rb
@@ -1,8 +1,9 @@
+beerbottles = 0
 
 while beerbottles < 100
-	beerbottles = 0.next
+
 	beerbottles.to_s + " bottles of beer on the wall, " + beerbottles.to_s + " bottles of beer, take one down, pass it around " + beerbottles.to_s + " of beer on the wall"
-	
+	beerbottles.next
 
 if beerbottles == 100
 	break
