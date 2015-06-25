568cf54420676021a7ef3bf287f3216d8404b7f7
Cody Buffaloe <codylbuffaloe@gmail.com>
2015-06-24 20:26:47 -0400

    trial 2, moved variable inside loop

--- Code/PoppinBottles.rb
+++ Code/PoppinBottles.rb
@@ -1,7 +1,8 @@
-beerbottles = 1
+
 while beerbottles < 100
+	beerbottles = 0.next
 	beerbottles.to_s + " bottles of beer on the wall, " + beerbottles.to_s + " bottles of beer, take one down, pass it around " + beerbottles.to_s + " of beer on the wall"
-	beerbottles.next
+	
 
 if beerbottles == 100
 	break
