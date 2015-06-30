8ed4a3c0079dda0908ad59ba3e3f657efd8d0685
Cody Buffaloe <codylbuffaloe@gmail.com>
2015-06-30 12:31:38 -0400

    Dunno what the DS files are, but created BoringDate and trying a hardcode approach to 99bottles.

--- Code/PoppinBottles.rb
+++ Code/PoppinBottles.rb
@@ -1,9 +1,8 @@
-while true
-	puts "How many bottle of beer on the wall?"
-	beerbottles = gets.chomp
-	puts beerbottles + " of beer on the wall, " + beerbottles + " . Take one down, pass it around " + beerbottles.to_i.pred.to_s + " bottles of beer on the wall."
 
-	if beerbottles == 0
-		break
-	end
+beerbottles = 99
+while beerbottles < 1
+	
+	puts beerbottles + " of beer on the wall, " + beerbottles + " . Take one down, pass it around " + beerbottles.to_i.pred.to_s + " bottles of beer on the wall."
+ 	beerbottles = beerbottles.to_i.pred
+	
 end
